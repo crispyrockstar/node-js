7269b0b9186dbbbcfe49044c72a1f5368f26557c
abhishek <crispyrockstar@gmail.com>
2024-12-26 00:11:59 +0530

    adding file copy

--- streams/01_writeStrems.js
+++ streams/01_writeStrems.js
@@ -1,4 +1,10 @@
 import fs from "fs";
 const writeStream = fs.createWriteStream("file.txt");
 
+//write to the stream
 writeStream.write("abc");
+
+//always end the stream for telling the system that the stream is done
+writeStream.end();
+
+console.log("hi");
---
+++ streams/02_copyFileUsingSteams.js
@@ -0,0 +1,40 @@
+import fs from "fs";
+console.time();
+const readStream = fs.createReadStream("D:\\NodeJS\\source.mkv", {
+  highWaterMark: 1 * 1024 * 1024,
+});
+
+const writeStream = fs.createWriteStream("destination.mkv");
+
+readStream.on("data", (chunk) => {
+  const isEmpty = writeStream.write(chunk);
+  //check if the write stream is full or not
+  if (!isEmpty) {
+    // console.log("pausing to pass the data to write stream");
+    readStream.pause();
+  }
+});
+
+writeStream.on("drain", () => {
+  readStream.resume();
+  //   console.log("now data is drained");
+});
+
+writeStream.on("close", () => {
+  console.log("file is closed");
+});
+
+writeStream.on("error", (error) => {
+  console.log("error", error);
+});
+
+writeStream.on("finish", () => {
+  console.log("write stream is finished");
+});
+
+readStream.on("end", () => {
+  writeStream.end();
+  console.log("file is copied");
+  console.timeEnd();
+});
+// writeStream.end();
