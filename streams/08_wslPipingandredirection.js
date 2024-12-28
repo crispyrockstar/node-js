176d5686156e6b052a0f5a9f3ae8b7ce8f1dcfb7
abhishek <crispyrockstar@gmail.com>
2024-12-28 15:14:42 +0530

    wsl+number writing

---
+++ streams/08_wslPipingandredirection.js
@@ -0,0 +1,18 @@
+/* use of streams along with different process running in the systems
+
+example:
+
+we can take input from the user using process.stdin and read this and use the inputs and 
+process this data in js file and write the output to process.stdout and now this output stream can be use to pipe the data to another process using | operator in the terminal(Linux)
+
+
+like using spawn from child process we can spawn another process and pipe the output of the current process to the input of the spawned process
+example: output of js file can be used to open a tab in the chrome browser
+
+now there can be million combinations depending on the requirement
+and flow how to design
+
+some automation tools those similar kind of things to automate the process
+example: zapier, ifttt
+
+ca*/
---
+++ streams/10_writingNumbers.js
@@ -0,0 +1,27 @@
+import fs from "fs";
+
+// console.time();
+// for (let i = 1; i <= 5000; i++) {
+//   fs.appendFileSync("10_numbers.txt", `${i} `);
+// }
+
+// console.timeEnd();
+// time taken near by 2s
+
+// now using streams
+
+const writeStreams = fs.createWriteStream("10_numbers.txt");
+
+console.time();
+for (let i = 1; i <= 500000; i++) {
+  writeStreams.write(`${i} `);
+}
+writeStreams.end();
+
+// writeStreams.on("finish", () => {
+// });
+console.timeEnd();
+// time taken near by 25ms
+//streams are faster than normal file writing
+
+// why streams are faster than normal file writing
